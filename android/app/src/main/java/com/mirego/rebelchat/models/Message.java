0448e6a018669532f8995aa01683d237e011838c
David Landry <davidlandry93@gmail.com>
2016-03-12 20:41:24 -0500

    Added message model

---
+++ android/app/src/main/java/com/mirego/rebelchat/models/Message.java
@@ -0,0 +1,11 @@
+package com.mirego.rebelchat.models;
+
+public class Message {
+    public final String id;
+    public final String text;
+
+    public Message(String id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+}
